be0c334bc32be70abeec64731efa0e9126cb4421
Kyraleese <crystalruby@gmail.com>
2019-12-23 20:17:40 -0500

    Initial Commit

---
+++ src/test/java/AmazingTest.java
@@ -0,0 +1,118 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+import java.util.Random;
+
+public class AmazingTest  {
+
+    @Test
+    public void testSeed0size15x20() {
+        String expected =
+                "Amazing - Copyright by Creative Computing, Morristown, NJ\n" +
+                        "+--+--+--+--+--+--+--+--+--+--+  +--+--+--+--+\n" +
+                        "|  |           |        |     |     |        | \n" +
+                        "+  +  +  +  +  +  +--+  +  +  +  +--+  +--+  +\n" +
+                        "|     |  |  |  |     |     |     |     |  |  | \n" +
+                        "+--+--+  +  +--+  +  +--+--+--+--+  +  +  +  +\n" +
+                        "|     |  |        |  |              |  |     | \n" +
+                        "+  +  +  +--+--+--+  +  +  +--+--+--+--+--+--+\n" +
+                        "|  |     |        |     |  |     |        |  | \n" +
+                        "+--+--+  +--+  +--+  +--+  +  +  +  +--+  +  +\n" +
+                        "|     |        |        |  |  |     |     |  | \n" +
+                        "+  +  +--+--+--+  +--+  +  +  +--+--+  +--+  +\n" +
+                        "|  |              |     |  |  |     |  |     | \n" +
+                        "+  +--+--+--+--+--+--+  +  +  +  +--+  +--+  +\n" +
+                        "|        |           |  |  |  |     |  |     | \n" +
+                        "+  +--+  +--+  +  +  +  +  +  +--+  +  +  +--+\n" +
+                        "|  |     |     |  |  |  |  |     |  |  |  |  | \n" +
+                        "+  +  +--+  +--+  +  +  +  +--+  +  +  +  +  +\n" +
+                        "|  |        |     |  |  |        |  |  |  |  | \n" +
+                        "+  +--+  +--+  +--+  +  +--+--+--+  +  +  +  +\n" +
+                        "|     |     |  |  |  |  |     |        |     | \n" +
+                        "+--+  +--+  +  +  +  +--+  +  +  +--+--+  +--+\n" +
+                        "|  |     |  |     |     |  |  |     |  |     | \n" +
+                        "+  +--+  +--+--+--+  +  +  +  +--+  +  +  +  +\n" +
+                        "|     |     |     |  |  |  |     |  |  |  |  | \n" +
+                        "+  +  +--+  +  +  +--+  +  +--+  +  +  +  +  +\n" +
+                        "|  |        |  |     |  |     |     |  |  |  | \n" +
+                        "+  +--+--+  +--+  +  +  +--+  +--+--+  +  +--+\n" +
+                        "|  |     |        |  |     |     |     |     | \n" +
+                        "+  +  +--+--+--+--+  +  +--+  +  +  +--+--+  +\n" +
+                        "|     |           |  |     |  |  |  |     |  | \n" +
+                        "+  +--+  +--+--+--+  +--+  +  +  +  +  +  +  +\n" +
+                        "|  |                 |     |  |  |     |  |  | \n" +
+                        "+  +  +--+--+--+--+--+  +--+  +--+  +--+  +  +\n" +
+                        "|  |  |  |           |     |     |  |     |  | \n" +
+                        "+  +  +  +  +--+  +  +--+  +--+  +  +  +--+  +\n" +
+                        "|  |  |     |     |  |  |     |     |  |     | \n" +
+                        "+--+  +--+--+  +  +  +  +  +  +--+--+  +--+  +\n" +
+                        "|     |        |  |  |  |  |  |     |        | \n" +
+                        "+  +--+  +--+--+  +  +  +  +  +--+  +  +--+--+\n" +
+                        "|        |        |     |  |        |        | \n" +
+                        "+--+--+--+--+--+--+--+--+  +--+--+--+--+--+--+\n";
+
+        Amazing.random = new Random(0);
+        Amazing.doit(15, 20);
+
+        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
+    }
+
+    @Test
+    public void testSeed100size4x5() {
+        String expected =
+                "Amazing - Copyright by Creative Computing, Morristown, NJ\n" +
+                        "+--+--+  +--+\n" +
+                        "|     |     | \n" +
+                        "+  +--+  +  +\n" +
+                        "|  |     |  | \n" +
+                        "+  +  +--+  +\n" +
+                        "|  |  |     | \n" +
+                        "+  +  +  +  +\n" +
+                        "|  |  |  |  | \n" +
+                        "+  +--+  +  +\n" +
+                        "|  |  |  |  | \n" +
+                        "+--+--+  +--+\n";
+
+        Amazing.random = new Random(100);
+        Amazing.doit(4, 5);
+        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
+
+    }
+
+    @Test
+    public void testSeed100size1x1() {
+        String expected = "Amazing - Copyright by Creative Computing, Morristown, NJ\n";
+
+        Amazing.random = new Random(100);
+        Amazing.doit(1, 1);
+        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
+    }
+
+    @Test
+    public void testSeed100size2x2() {
+        String expected = "Amazing - Copyright by Creative Computing, Morristown, NJ\n" +
+                "+--+  +\n" +
+                "|     | \n" +
+                "+--+  +\n" +
+                "|     | \n" +
+                "+  +--+\n";
+
+        Amazing.random = new Random(100);
+        Amazing.doit(2, 2);
+        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
+    }
+
+    @Test
+    public void testSeed0size2x2() {
+        String expected = "Amazing - Copyright by Creative Computing, Morristown, NJ\n" +
+                "+--+  +\n" +
+                "|  |  | \n" +
+                "+  +--+\n" +
+                "|  |  | \n" +
+                "+--+--+\n";
+
+        Amazing.random = new Random(10);
+        Amazing.doit(2, 2);
+        assertEquals("Should have the maze that was expected", expected, Amazing.result.toString());
+    }
+}
